8872ad0dab4b8f9511f66b463bf4c194820e51ec
tacman <info@survos-ci.com>
2024-06-11 20:48:12 +0000

    Merge branch main of github.com:survos/survos

---
+++ src/Metadata/FlickrUserInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Survos\FlickrBundle\Metadata;
+
+interface FlickrUserInterface
+{
+    public function getFlickrKey(): ?string;
+    public function getFlickrSecret(): ?string;
+    public function getFlickrUserId(): ?string;
+}
+
---
+++ src/Metadata/FlickrUserTrait.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Survos\FlickrBundle\Metadata;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait FlickrUserTrait
+{
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $flickrUserId = null; // e.g. "26016159@N00"
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $flickrKey = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $flickrSecret = null;
+
+    public function getFlickrSecret(): ?string
+    {
+        return $this->flickrSecret;
+    }
+
+
+    public function getFlickrKey(): ?string
+    {
+        return $this->flickrKey;
+    }
+
+    public function setFlickrKey(?string $flickrKey): static
+    {
+        $this->flickrKey = $flickrKey;
+
+        return $this;
+    }
+
+    public function setFlickrSecret(?string $flickrSecret): static
+    {
+        $this->flickrSecret = $flickrSecret;
+
+        return $this;
+    }
+
+    public function getFlickrUserId(): ?string
+    {
+        return $this->flickrUserId;
+    }
+
+    public function setFlickrUserId(?string $flickrUserId): self
+    {
+        $this->flickrUserId = $flickrUserId;
+        return $this;
+    }
+
+
+
+}
